2e2a54217ab37c3f63532e69bab739855b2ee302
Jake <45055732+JakeHelsel72@users.noreply.github.com>
2023-06-18 23:07:57 -0500

    render the board by offset with hitreg fixed

--- ChessMain.py
+++ ChessMain.py
@@ -23,31 +23,39 @@ def load_images():
 def main():
     os.listdir()
     p.init()
-    screen = p.display.set_mode((WIDTH, HEIGHT))
+    load_images()
+    
+    #global vars
+    screen = p.display.set_mode((WIDTH+100, HEIGHT+100))
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     board = chess.Board()
-    load_images()
+    board_offset_x = board_offset_y = 100
+    flip_board = False
     running = True
     sq_selected = [] # keeps track of square selected
-    player_clicks = []
+    player_clicks = [] #keeps track of where the player has clicked
+
     while running:
         for e in p.event.get():
-            if e.type == p.QUIT:
-                running = False
-            elif e.type == p.MOUSEBUTTONDOWN:
-                location = p.mouse.get_pos() #(x, y) of mouse
-                col = location[0]//SQ_SIZE
-                row = location[1]//SQ_SIZE
-                #if same square is selected, clears click que
-                if len(sq_selected) > 0 and sq_selected[0] == (col+(row*8)):
-                    sq_selected = []
+            if e.type == p.QUIT: #when the x of the window is pressed,
+                running = False #stops running 
+            elif e.type == p.K_f: #when Key "f" is pressed,
+                flip_board = not flip_board #changes to opposite
+            elif e.type == p.MOUSEBUTTONDOWN: #when mouse is pressed,
+                location = p.mouse.get_pos() # gets (x, y) tuple of mouse position
+                print(f"{location=}") #prints "location"s value to console
+                col = (location[0] - board_offset_x)//SQ_SIZE #converts x to number of square from the left it is
+                row = (location[1] - board_offset_y)//SQ_SIZE #converts y to number of square from the top it is
+                if len(sq_selected) > 0 and sq_selected[0] == (col+(row*8)): #if same square is selected, 
+                    sq_selected = [] #clears click queue
                     player_clicks = []
-                else: #this is when the first click has already been registered
-                    sq_selected.append(col+row*8) 
+                else: #distinct square is chosen,
+                    sq_selected.append(col+row*8) #figure out chess import's square number
                     player_clicks.append(sq_selected)
                     print('click registered')
-                if len(player_clicks) == 2:
+                if len(player_clicks) == 2: #when two squares are selected,
+                    print(f'{sq_selected=}')
                     move = chess.Move(sq_selected[0], sq_selected[1])
                     if board.is_legal(move):
                         board.push(move)
@@ -59,21 +67,26 @@ def main():
                         sq_selected = []
                         player_clicks = []
 
-        draw_gamestate(screen, board)
+        draw_gamestate(screen, board, board_offset_x, board_offset_y)
+        myfont = p.font.SysFont("monospace", 20)
+        BLACK = (0, 0, 0)
+        label = myfont.render("press f to flip board", 1, BLACK)
+        screen.blit(label, (20, 10))
         clock.tick(MAX_FPS)
         p.display.flip()
 
 
-def draw_gamestate(screen, board):
-    draw_board(screen)
-    draw_pieces(screen, board)
+def draw_gamestate(screen, board, x=0, y=0):
+    draw_board(screen, x, y)
+    draw_pieces(screen, board, x, y)
 
-def draw_board(screen):
+def draw_board(screen, x=0, y=0):
+    #x and y are offset
     colors = [p.Color("white"), p.Color('gray')]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             color = colors[((r+c)%2)]
-            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE+x, r*SQ_SIZE+y, SQ_SIZE, SQ_SIZE))
 
 #changes chess.py piece to filename piece (B (white bishop) -> B, k (black king) -> bK)
 def official_to_unofficial(official):
@@ -82,13 +95,14 @@ def official_to_unofficial(official):
         return 'b'+str(official).upper()
     return str(official)
 
-def draw_pieces(screen, board):
+def draw_pieces(screen, board, x=0, y=0):
+    #x and y are offset
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             official_piece = board.piece_at(c+(r*8))
             file_piece = official_to_unofficial(official_piece)
             if file_piece != 'None':
-                screen.blit(IMAGES[file_piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+                screen.blit(IMAGES[file_piece], p.Rect(c*SQ_SIZE+x, r*SQ_SIZE+y, SQ_SIZE, SQ_SIZE))
 
 
 if __name__ == "__main__":
